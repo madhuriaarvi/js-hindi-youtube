c275e6ba37d430ae1c764cc9a37512071b6e78f9
madhuriaarvi <91321294+madhuriaarvi@users.noreply.github.com>
2024-08-23 13:44:39 +0000

    All about datatypes

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -42,3 +42,28 @@ let someNumber = 33
 let stringNumber = String(someNumber)
 console.log(stringNumber);
 console.log(typeof(stringNumber));
+
+
+
+//********************** OPERATIONS *********************** */
+
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+// console.log(2+2);
+// console.log(2*2);
+
+
+let str1 = "hello"
+let str2 = " madhuri"
+let str3 = str1 + str2
+console.log(str3)
+
+console.log("1" +2 +2)
+console.log(1 +2 +"2")
+
+let x =3
+// let y = x++
+let y = ++x
+console.log(x + "," + y)
---
+++ 01_basic/datatypesSummary.js
@@ -0,0 +1,21 @@
+
+
+
+
+// primitive - call by value
+
+// String
+// Number
+// Boolean
+// null
+// undefined
+// Symbol
+// BigInt
+
+
+
+// non-primitive - call by reference
+
+// arrays
+// objects
+// functions
