07ca9cfc658271457892833b91aa80fa0474416d
madhuriaarvi <91321294+madhuriaarvi@users.noreply.github.com>
2024-09-02 13:26:59 +0000

    arrays

---
+++ 02_basic/01_arrays.js
@@ -0,0 +1,19 @@
+
+
+const myArray = [0, 1, 2, 3, 4]
+// const myArray1 = [0, 1, 2, hdsf, Demo]
+
+const cartoon = ["mickey", "donld","popai"]
+
+const myArray2 = new Array(1,2,3,4)
+
+
+//Accessing
+
+console.log(myArray[0]);
+
+// myArray2.push(6)
+// myArray2.push(7)
+// console.log(myArray2);
+
+// myArray2.pop(7)
